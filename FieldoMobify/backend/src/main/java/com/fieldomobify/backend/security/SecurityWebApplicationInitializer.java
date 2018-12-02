8c5e6d7d144c73ca0dd5fe53251337d88bf7bf84
haiphong <haiphong238@gmail.com>
2018-12-03 00:13:14 +0700

    Remove unused code

--- FieldoMobify/backend/src/main/java/com/fieldomobify/backend/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.fieldomobify.backend.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
