7cb6f196e773db896565c708ccfee2d13a2a9f9a
haiphong <haiphong238@gmail.com>
2018-12-03 00:08:15 +0700

    Add new APIs for Organization, Job

--- FieldoMobify/backend/src/main/java/com/fieldomobify/backend/Application.java
+++ FieldoMobify/backend/src/main/java/com/fieldomobify/backend/Application.java
@@ -2,16 +2,27 @@ package com.fieldomobify.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import com.fieldomobify.backend.events.CascadeSaveMongoEventListener;
+
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.fieldomobify.backend" }, excludeFilters = { @Filter(type = FilterType.ANNOTATION, value = Configuration.class) })
+@Configuration
+@ComponentScan(basePackages = { "com.fieldomobify.backend" }, excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = Configuration.class) })
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public CascadeSaveMongoEventListener userCascadingMongoEventListener() {
+		return new CascadeSaveMongoEventListener();
+	}
+
 }
