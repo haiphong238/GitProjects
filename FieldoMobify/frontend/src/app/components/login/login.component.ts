8dd59e04b3c8b1b51d039eed2b334c559550e74f
haiphong238 <haiphong238@gmail.com>
2018-12-03 17:31:37 +0700

    Fix UI bug: Login and Sign up page are not displayed fully.

--- FieldoMobify/frontend/src/app/components/login/login.component.ts
+++ FieldoMobify/frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AccountService } from 'src/app/services/account.service';
 import { Observable } from 'rxjs';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +15,7 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   validMessage: string = "";
+  height: string = "800px";
 
   model: any = {};
 
@@ -28,21 +29,28 @@ export class LoginComponent implements OnInit {
     });
 
     sessionStorage.setItem('token', '');
+
+    this.height = `${window.innerHeight}px`;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  resize(event: any) {
+    this.height = `${event.target.innerHeight}px`;
   }
 
   submitLogin() {
     let url = '/server/login';
-        this.http.post<Observable<boolean>>(url, {
-            userName: this.model.username,
-            password: this.model.password
-        }).subscribe(isValid => {
-            if (isValid) {
-                sessionStorage.setItem('token', btoa(this.model.username + ':' + this.model.password));
-                this.router.navigate(['/home']);
-            } else {
-                alert("Authentication failed.")
-            }
-        });
+    this.http.post<Observable<boolean>>(url, {
+      userName: this.model.username,
+      password: this.model.password
+    }).subscribe(isValid => {
+      if (isValid) {
+        sessionStorage.setItem('token', btoa(this.model.username + ':' + this.model.password));
+        this.router.navigate(['/home']);
+      } else {
+        alert("Authentication failed.")
+      }
+    });
 
   }
 
